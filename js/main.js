7fc8468d494656f570f291f14dd24892dbb699d2
blackchoi <balmein23@gmail.com>
2022-10-28 09:47:20 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(function () {
+
+
+
+    $('.listbox').on('click', function (e) {
+        e.preventDefault();
+        $('.pop').show();
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); // html, css문서 다 읽고 제일 마지막에 읽으라는 opening 구문
